9841d33eacc7c51513a3c2b6807d386722827ee1
Tanmay Aseri <87698651+Tanmay-Aseri@users.noreply.github.com>
2023-01-12 15:39:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+
+function draw(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    background("white");
+}
+
+function clearCanvas(){
+    background("white");
+}
